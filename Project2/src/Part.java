e881133af59e28bf2d33d6e8000d6bd1500017c4
Cole Busby <cole.busby@oportun.com>
2020-03-30 12:15:58 -0700

    single assembly fixes

--- Project2/src/Part.java
+++ Project2/src/Part.java
@@ -2,55 +2,55 @@ import java.util.ArrayList;
 
 public class Part {
 
-        //local variables
-            private String partNumber;
-            private String serialNumber;
-            private int weight;
-            private double surfaceArea;
+    //local variables
+    private String partNumber;
+    private String serialNumber;
+    private int weight;
+    private double surfaceArea;
 
 
-                public Part(String partNumber, String serialNumber, int weight, double surfaceArea) {
-                    this.partNumber = partNumber;
-                    this.serialNumber = serialNumber;
-                    this.weight = weight;
-                    this.surfaceArea = surfaceArea;
-                }
+    public Part(String partNumber, String serialNumber, int weight, double surfaceArea) {
+        this.partNumber = partNumber;
+        this.serialNumber = serialNumber;
+        this.weight = weight;
+        this.surfaceArea = surfaceArea;
+    }
 
     public Part(ArrayList<Part> parts) {
     }
 
 
     public String getPartNumber() {
-                    return partNumber;
-                }
+        return partNumber;
+    }
 
-                    public void setPartNumber(String partNumber) {
-                        this.partNumber = partNumber;
-                    }
+    public void setPartNumber(String partNumber) {
+        this.partNumber = partNumber;
+    }
 
-                public String getSerialNumber() {
-                    return serialNumber;
-                }
+    public String getSerialNumber() {
+        return serialNumber;
+    }
 
-                    public void setSerialNumber(String serialNumber) {
-                        this.serialNumber = serialNumber;
-                    }
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
 
-                public int getWeight() {
-                    return weight;
-                }
+    public int getWeight() {
+        return weight;
+    }
 
-                    public void setWeight(int weight) {
-                        this.weight = weight;
-                    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 
-                public double getSurfaceArea() {
-                    return surfaceArea;
-                }
+    public double getSurfaceArea() {
+        return surfaceArea;
+    }
 
-                    public void setSurfaceArea(double surfaceArea) {
-                        this.surfaceArea = surfaceArea;
-                    }
+    public void setSurfaceArea(double surfaceArea) {
+        this.surfaceArea = surfaceArea;
+    }
 
     @Override
     public String toString() {
